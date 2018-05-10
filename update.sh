72110d3db85400f1933bf39c79cc4df9fbeb0baa
Matheus Souza <matheus.souzadv@gmail.com>
2018-05-09 23:26:41 -0300

    add .sh to update public_html folder

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git pull
+mv  /home/u706997718/public_html/index.php index.php.saved
+rm -rf /home/u706997718/public_html/*
+# update project/ to your directory name
+cp -a /home/u706997718/public/* /home/u706997718/public_html
+cp /home/u706997718/public/.htaccess*  /home/u706997718/public_html
+rm -rf  /home/u706997718/public_htm/index.php
+mv index.php.saved www/index.php
