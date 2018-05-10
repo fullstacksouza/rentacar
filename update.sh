2d2967737412002953304922d97cb52f1a2cee68
Matheus Souza <matheus.souzadv@gmail.com>
2018-05-09 23:28:08 -0300

    add .sh to update public_html folder

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ rm -rf /home/u706997718/public_html/*
 cp -a /home/u706997718/public/* /home/u706997718/public_html
 cp /home/u706997718/public/.htaccess*  /home/u706997718/public_html
 rm -rf  /home/u706997718/public_htm/index.php
-mv index.php.saved www/index.php
+mv index.php.saved  /home/u706997718/public_html/index.php
