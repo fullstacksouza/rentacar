6098aec47495bc7d13cba66420e59546605fd9f6
Matheus Souza <matheussouza@infocast.com.br>
2018-06-06 09:00:20 -0300

    update sector request

---
+++ app/Http/Requests/UpdateSectorRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSectorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+     public function rules()
+     {
+         return [
+             'name' => 'required',
+             'responsible_email' =>'email|required'
+
+         ];
+     }
+
+     public function messages()
+     {
+         return [
+             'name.required' => "O campo nome é obrigatório",
+             'email.email'=>"Digite um email válido",
+             'responsible_email.email' => "Digite um email valido",
+             'responsible_email.required'=>"O campo email nao pode ser vazio"
+         ];
+     }
+}
